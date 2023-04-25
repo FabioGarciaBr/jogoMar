f9798ceee2257614944f49c2478ddcc5f739f104
FabioGarciaBr <129271590+FabioGarciaBr@users.noreply.github.com>
2023-04-25 06:36:07 -0300

    create

---
+++ js/script.js
@@ -0,0 +1,53 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+function jump() {
+  mario.classList.add('jump');
+  setTimeout(() => {
+    mario.classList.remove('jump');
+  }, 500);
+}
+
+function checkCollision() {
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+  if (pipePosition <= 50 && pipePosition > 0 && marioPosition < 80) {
+    stopGame();
+  }
+}
+
+function checkCollision() {
+  const MAX_WIDTH = 768;
+  const pipePosition = pipe.offsetLeft * (window.innerWidth / MAX_WIDTH);
+  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '') * (window.innerWidth / MAX_WIDTH);
+
+  if (pipePosition <= 40 && pipePosition > 0 && marioPosition < 40) {
+    stopGame();
+  }
+}
+
+
+function stopGame() {
+  pipe.style.animation = 'none';
+  pipe.style.left = `${pipe.offsetLeft}px`;
+
+  const clouds = document.querySelectorAll('.cloud, .clouds, .bad');
+  clouds.forEach(cloud => {
+    cloud.style.animation = 'none';
+    cloud.style.left = `${cloud.offsetLeft}px`;
+  });
+
+  mario.style.animation = 'none';
+  mario.style.bottom = `${mario.style.bottom}`;
+  mario.src= 'imagens/loser.png';
+  mario.style.width = '40px';
+
+  clearInterval(loop);
+}
+
+const loop = setInterval(() => {
+  checkCollision();
+}, 10);
+
+document.addEventListener('keydown', jump);
---
+++ song.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+  var meuAudio = document.getElementById("meuAudio"); 
+  meuAudio.play(); 
+});
