433babc9d90f0379e7c1ab115282e7af29d96a0b
FabioGarciaBr <129271590+FabioGarciaBr@users.noreply.github.com>
2023-04-26 16:26:10 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,91 +1,71 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const cloudbad = document.querySelector('.cloudbad');
 
+//pulo
 function jump() {
   mario.classList.add('jump');
   setTimeout(() => {
     mario.classList.remove('jump');
   }, 500);
-}
 
-function checkCollision() {
-  const pipePosition = pipe.offsetLeft;
-  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
-
-  if (pipePosition <= 50 && pipePosition > 0 && marioPosition < 80) {
+  const cloudbadPosition = +window.getComputedStyle(cloudbad).left.replace('px', '');
+  if (cloudbadPosition <= 50 && cloudbadPosition > 0) {
     stopGame();
   }
 }
 
+
 function checkCollision() {
-  const MAX_WIDTH = 900;
+  const MAX_WIDTH = 768;
   const pipePosition = pipe.offsetLeft * (window.innerWidth / MAX_WIDTH);
   const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '') * (window.innerWidth / MAX_WIDTH);
-
-  if (pipePosition <= 20 && pipePosition > 0 && marioPosition < 20) {
+ 
+  if (pipePosition <= 30 && pipePosition > 0 && marioPosition < 30) {
     stopGame();
   }
 }
-const isMobile = window.matchMedia("(max-width: 900px)").matches;
+
+const isMobile = window.matchMedia("(max-width: 768px)").matches;
 
 if (isMobile) {
   document.addEventListener('touchstart', jump);
 }
 
-
 function stopGame() {
-  pipe.style.animation = 'none';
+  clearInterval(loop); // Para o loop de verificação de colisão
+  pipe.classList.add('paused'); // Pausa a animação do cano
   pipe.style.left = `${pipe.offsetLeft}px`;
-
-  const clouds = document.querySelectorAll('.cloud, .clouds, .bad');
-  clouds.forEach(cloud => {
-    cloud.style.animation = 'none';
-    cloud.style.left = `${cloud.offsetLeft}px`;
-  });
-
-  mario.style.animation = 'none';
+  mario.classList.add('paused'); // Pausa a animação do Mario
   mario.style.bottom = `${mario.style.bottom}`;
   mario.src= 'imagens/loser.png';
   mario.style.width = '40px';
 
-  function stopGame() {
-  clearInterval(gameInterval);
-  pipe.classList.add('paused');
-  mario.classList.add('paused');
+  const clouds = document.querySelectorAll('.cloud, .clouds, .cloudbad');
+  clouds.forEach(cloud => {
+    cloud.style.animation = 'none';
+    cloud.style.left = `${cloud.offsetLeft}px`;
+  });
   
   // Pare o autoplay do elemento de áudio
   var meuAudio = document.getElementById("meuAudio");
   meuAudio.pause();
-}
-  clearInterval(loop);
-}
 
+  // Exibe a mensagem de derrota
+  const gameOver = document.createElement('div');
+  gameOver.classList.add('game-over');
+  gameOver.innerHTML = '<img src="imagens/loser.png" width="40"><p>Você perdeu!</p><button onclick="location.reload()">Jogar novamente</button>';
+  document.body.appendChild(gameOver);
+  gameOver.style.position = 'absolute';
+  gameOver.style.left = '50%';
+  gameOver.style.top = '50%';
+  gameOver.style.transform = 'translate(-50%, -50%)';
+
+;
+
+}
 const loop = setInterval(() => {
   checkCollision();
 }, 10);
 
 document.addEventListener('keydown', jump);
-
-
-// elemento de botão
-const reloadBtn = document.createElement('button');
-
-//texto do botão
-reloadBtn.innerText = 'Recarregar';
-
-// estilo do botão
-reloadBtn.style.padding = '10px';
-reloadBtn.style.backgroundColor = '#4CAF50';
-reloadBtn.style.color = 'white';
-reloadBtn.style.border = 'none';
-reloadBtn.style.borderRadius = '5px';
-reloadBtn.style.cursor = 'pointer';
-
-
-reloadBtn.addEventListener('click', function() {
-  location.reload();
-});
-
-
-document.body.appendChild(reloadBtn);
-
